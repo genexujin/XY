514e20a027ef4c6a3d8ad9bcb9e5bc6ca4fa847e
Jin <xxx>
2013-01-12 14:13:51 +0800

    Database Script, initial version, includes only basic infra tables

---
+++ Database/OA.sql
@@ -0,0 +1,459 @@
+DROP USER OA;
+CREATE USER OA IDENTIFIED BY XIANGYUN;
+GRANT CONNECT,RESOURCE TO OA;
+
+
+--------------------------------------------------------
+--  File created - Saturday-January-12-2013   
+--------------------------------------------------------
+  DROP TABLE "OA"."ANNOUNCEMENTS" cascade constraints;
+  DROP TABLE "OA"."DEPARTMENTS" cascade constraints;
+  DROP TABLE "OA"."EMPLOYEES" cascade constraints;
+  DROP TABLE "OA"."LOCATIONS" cascade constraints;
+  DROP TABLE "OA"."LOVS" cascade constraints;
+  DROP TABLE "OA"."MENUS" cascade constraints;
+  DROP TABLE "OA"."NOTIFICATIONS" cascade constraints;
+  DROP TABLE "OA"."ROLES" cascade constraints;
+  DROP TABLE "OA"."ROLE_MENUS" cascade constraints;
+  DROP TABLE "OA"."ROLE_USERS" cascade constraints;
+  DROP SEQUENCE "OA"."ANNOUNCEMENTS_SEQ";
+  DROP SEQUENCE "OA"."DEPARTMENT_SEQ";
+  DROP SEQUENCE "OA"."EMPLOYEES_SEQ";
+  DROP SEQUENCE "OA"."LOCATIONS_SEQ";
+  DROP SEQUENCE "OA"."LOVS_SEQ";
+  DROP SEQUENCE "OA"."NOTIFICATIONS_SEQ";
+  DROP SEQUENCE "OA"."ROLES_SEQ";
+--------------------------------------------------------
+--  DDL for Sequence ANNOUNCEMENTS_SEQ
+--------------------------------------------------------
+
+   CREATE SEQUENCE  "OA"."ANNOUNCEMENTS_SEQ"  MINVALUE 1 MAXVALUE 9999999999999999999999999999 INCREMENT BY 1 START WITH 1 CACHE 20 NOORDER  NOCYCLE ;
+--------------------------------------------------------
+--  DDL for Sequence DEPARTMENT_SEQ
+--------------------------------------------------------
+
+   CREATE SEQUENCE  "OA"."DEPARTMENT_SEQ"  MINVALUE 1 MAXVALUE 9999999999999999999999999999 INCREMENT BY 1 START WITH 1 CACHE 20 NOORDER  NOCYCLE ;
+--------------------------------------------------------
+--  DDL for Sequence EMPLOYEES_SEQ
+--------------------------------------------------------
+
+   CREATE SEQUENCE  "OA"."EMPLOYEES_SEQ"  MINVALUE 1 MAXVALUE 9999999999999999999999999999 INCREMENT BY 1 START WITH 1 CACHE 20 NOORDER  NOCYCLE ;
+--------------------------------------------------------
+--  DDL for Sequence LOCATIONS_SEQ
+--------------------------------------------------------
+
+   CREATE SEQUENCE  "OA"."LOCATIONS_SEQ"  MINVALUE 1 MAXVALUE 9999999999999999999999999999 INCREMENT BY 1 START WITH 1 CACHE 20 NOORDER  NOCYCLE ;
+--------------------------------------------------------
+--  DDL for Sequence LOVS_SEQ
+--------------------------------------------------------
+
+   CREATE SEQUENCE  "OA"."LOVS_SEQ"  MINVALUE 1 MAXVALUE 9999999999999999999999999999 INCREMENT BY 1 START WITH 1 CACHE 20 NOORDER  NOCYCLE ;
+--------------------------------------------------------
+--  DDL for Sequence NOTIFICATIONS_SEQ
+--------------------------------------------------------
+
+   CREATE SEQUENCE  "OA"."NOTIFICATIONS_SEQ"  MINVALUE 1 MAXVALUE 9999999999999999999999999999 INCREMENT BY 1 START WITH 1 CACHE 20 NOORDER  NOCYCLE ;
+--------------------------------------------------------
+--  DDL for Sequence ROLES_SEQ
+--------------------------------------------------------
+
+   CREATE SEQUENCE  "OA"."ROLES_SEQ"  MINVALUE 1 MAXVALUE 9999999999999999999999999999 INCREMENT BY 1 START WITH 1 CACHE 20 NOORDER  NOCYCLE ;
+--------------------------------------------------------
+--  DDL for Table ANNOUNCEMENTS
+--------------------------------------------------------
+
+  CREATE TABLE "OA"."ANNOUNCEMENTS" 
+   (	"ID" VARCHAR2(20 BYTE), 
+	"SENDER_ID" VARCHAR2(20 BYTE), 
+	"SENDER_NAME" VARCHAR2(20 BYTE), 
+	"TITLE" VARCHAR2(200 BYTE), 
+	"CONTENT" VARCHAR2(2000 BYTE), 
+	"ATTACHMENT_PATH" VARCHAR2(100 BYTE), 
+	"SENT_DATE" DATE, 
+	"EXPIRE_DATE" DATE, 
+	"CATEGORY" VARCHAR2(20 BYTE), 
+	"IMPORTANCE" VARCHAR2(20 BYTE)
+   ) ;
+--------------------------------------------------------
+--  DDL for Table DEPARTMENTS
+--------------------------------------------------------
+
+  CREATE TABLE "OA"."DEPARTMENTS" 
+   (	"DEPT_ID" VARCHAR2(20 BYTE), 
+	"DEPT_NAME" VARCHAR2(50 BYTE), 
+	"LOCATION_ID" VARCHAR2(20 BYTE), 
+	"MGR_ID" VARCHAR2(20 BYTE), 
+	"CREATED_AT" DATE, 
+	"CREATED_BY" VARCHAR2(20 BYTE), 
+	"LAST_UPDATED_AT" DATE, 
+	"LAST_UPDATED_BY" VARCHAR2(20 BYTE)
+   ) ;
+--------------------------------------------------------
+--  DDL for Table EMPLOYEES
+--------------------------------------------------------
+
+  CREATE TABLE "OA"."EMPLOYEES" 
+   (	"USER_NAME" VARCHAR2(20 BYTE), 
+	"PASSWORD" VARCHAR2(20 BYTE), 
+	"FIRST_NAME" VARCHAR2(20 BYTE), 
+	"LAST_NAME" VARCHAR2(20 BYTE), 
+	"GENDER" VARCHAR2(20 BYTE), 
+	"MGR_ID" VARCHAR2(20 BYTE), 
+	"DEPT_ID" VARCHAR2(20 BYTE), 
+	"EMAIL" VARCHAR2(100 BYTE), 
+	"MOBILE" VARCHAR2(20 BYTE), 
+	"OFFICE_PHONE" VARCHAR2(20 BYTE), 
+	"HOME_PHONE" VARCHAR2(20 BYTE), 
+	"FAX" VARCHAR2(20 BYTE), 
+	"LOCATION_ID" VARCHAR2(20 BYTE), 
+	"HOME_ADDRESS" VARCHAR2(20 BYTE), 
+	"OFFICE_ADDRESS" VARCHAR2(20 BYTE), 
+	"PHOTO" BLOB, 
+	"LAST_UPDATED_BY" VARCHAR2(20 BYTE), 
+	"LAST_UPDATED_AT" DATE, 
+	"CREATED_BY" VARCHAR2(20 BYTE), 
+	"CREATED_AT" DATE, 
+	"TITLE" VARCHAR2(20 BYTE), 
+	"ID" VARCHAR2(20 BYTE)
+   ) 
+ LOB ("PHOTO") STORE AS BASICFILE "SYS_LOB0000020052C00016$$"(ENABLE STORAGE IN ROW CHUNK 8192 RETENTION 
+  NOCACHE LOGGING ) ;
+--------------------------------------------------------
+--  DDL for Table LOCATIONS
+--------------------------------------------------------
+
+  CREATE TABLE "OA"."LOCATIONS" 
+   (	"LOCATION_ID" VARCHAR2(20 BYTE), 
+	"FAX" VARCHAR2(20 BYTE), 
+	"STREET_ADDRESS" VARCHAR2(200 BYTE), 
+	"POSTAL_CODE" VARCHAR2(20 BYTE), 
+	"LOCATION_NAME" VARCHAR2(20 BYTE), 
+	"FRONT_DESK_PHONE" VARCHAR2(20 BYTE), 
+	"CREATED_AT" DATE, 
+	"CREATED_BY" VARCHAR2(20 BYTE), 
+	"LAST_UPDATED_AT" DATE, 
+	"LAST_UPDATED_BY" VARCHAR2(20 BYTE)
+   ) ;
+--------------------------------------------------------
+--  DDL for Table LOVS
+--------------------------------------------------------
+
+  CREATE TABLE "OA"."LOVS" 
+   (	"ID" VARCHAR2(20 BYTE), 
+	"CODE" VARCHAR2(20 BYTE), 
+	"LOV_DESC" VARCHAR2(200 BYTE), 
+	"PARENT_CODE" VARCHAR2(20 BYTE), 
+	"VALUE" VARCHAR2(20 BYTE), 
+	"CREATED_AT" DATE, 
+	"CREATED_BY" VARCHAR2(20 BYTE), 
+	"LAST_UPDATED_BY" VARCHAR2(20 BYTE), 
+	"LAST_UPDATED_AT" DATE
+   ) ;
+--------------------------------------------------------
+--  DDL for Table MENUS
+--------------------------------------------------------
+
+  CREATE TABLE "OA"."MENUS" 
+   (	"MENU_ID" VARCHAR2(20 BYTE), 
+	"MENU_NAME" VARCHAR2(20 BYTE), 
+	"MENU_TASKFLOW_URL" VARCHAR2(500 BYTE), 
+	"MENU_DESC" VARCHAR2(20 BYTE), 
+	"MENU_CATEGORY" VARCHAR2(20 BYTE), 
+	"PARENT_MENU_ID" VARCHAR2(20 BYTE)
+   ) ;
+--------------------------------------------------------
+--  DDL for Table NOTIFICATIONS
+--------------------------------------------------------
+
+  CREATE TABLE "OA"."NOTIFICATIONS" 
+   (	"ID" VARCHAR2(20 BYTE), 
+	"CATEGORY" VARCHAR2(20 BYTE), 
+	"PRIORITY" VARCHAR2(20 BYTE), 
+	"TITLE" VARCHAR2(200 BYTE), 
+	"CONTENT" VARCHAR2(2000 BYTE), 
+	"TO_USER_ID" VARCHAR2(20 BYTE), 
+	"EVENT_DATE" DATE, 
+	"IS_SMS_SENT" VARCHAR2(20 BYTE)
+   ) ;
+--------------------------------------------------------
+--  DDL for Table ROLES
+--------------------------------------------------------
+
+  CREATE TABLE "OA"."ROLES" 
+   (	"ROLE_ID" VARCHAR2(20 BYTE), 
+	"ROLE_NAME" VARCHAR2(50 BYTE), 
+	"ROLE_DESC" VARCHAR2(200 BYTE), 
+	"CREATED_AT" DATE, 
+	"CREATED_BY" VARCHAR2(20 BYTE), 
+	"LAST_UPDATED_AT" DATE, 
+	"LAST_UPDATED_BY" VARCHAR2(20 BYTE)
+   ) ;
+--------------------------------------------------------
+--  DDL for Table ROLE_MENUS
+--------------------------------------------------------
+
+  CREATE TABLE "OA"."ROLE_MENUS" 
+   (	"ROLE_ID" VARCHAR2(20 BYTE), 
+	"MENU_ID" VARCHAR2(20 BYTE), 
+	"CREATED_BY" VARCHAR2(20 BYTE), 
+	"CREATED_AT" DATE
+   ) ;
+--------------------------------------------------------
+--  DDL for Table ROLE_USERS
+--------------------------------------------------------
+
+  CREATE TABLE "OA"."ROLE_USERS" 
+   (	"ROLE_ID" VARCHAR2(20 BYTE), 
+	"USER_ID" VARCHAR2(20 BYTE), 
+	"CREATED_AT" DATE, 
+	"CREATED_BY" VARCHAR2(20 BYTE)
+   ) ;
+REM INSERTING into OA.ANNOUNCEMENTS
+REM INSERTING into OA.DEPARTMENTS
+REM INSERTING into OA.EMPLOYEES
+REM INSERTING into OA.LOCATIONS
+REM INSERTING into OA.LOVS
+REM INSERTING into OA.MENUS
+REM INSERTING into OA.NOTIFICATIONS
+REM INSERTING into OA.ROLES
+REM INSERTING into OA.ROLE_MENUS
+REM INSERTING into OA.ROLE_USERS
+--------------------------------------------------------
+--  DDL for Index ROLE_USERS_PK
+--------------------------------------------------------
+
+  CREATE UNIQUE INDEX "OA"."ROLE_USERS_PK" ON "OA"."ROLE_USERS" ("ROLE_ID", "USER_ID") 
+  ;
+--------------------------------------------------------
+--  DDL for Index DEPARTMENT_PK
+--------------------------------------------------------
+
+  CREATE UNIQUE INDEX "OA"."DEPARTMENT_PK" ON "OA"."DEPARTMENTS" ("DEPT_ID") 
+  ;
+--------------------------------------------------------
+--  DDL for Index ANNOUNCEMENTS_PK
+--------------------------------------------------------
+
+  CREATE UNIQUE INDEX "OA"."ANNOUNCEMENTS_PK" ON "OA"."ANNOUNCEMENTS" ("ID") 
+  ;
+--------------------------------------------------------
+--  DDL for Index LOVS_PK
+--------------------------------------------------------
+
+  CREATE UNIQUE INDEX "OA"."LOVS_PK" ON "OA"."LOVS" ("ID") 
+  ;
+--------------------------------------------------------
+--  DDL for Index LOCATIONS_PK
+--------------------------------------------------------
+
+  CREATE UNIQUE INDEX "OA"."LOCATIONS_PK" ON "OA"."LOCATIONS" ("LOCATION_ID") 
+  ;
+--------------------------------------------------------
+--  DDL for Index ROLE_MENUS_PK
+--------------------------------------------------------
+
+  CREATE UNIQUE INDEX "OA"."ROLE_MENUS_PK" ON "OA"."ROLE_MENUS" ("ROLE_ID", "MENU_ID") 
+  ;
+--------------------------------------------------------
+--  DDL for Index NOTIFICATIONS_PK
+--------------------------------------------------------
+
+  CREATE UNIQUE INDEX "OA"."NOTIFICATIONS_PK" ON "OA"."NOTIFICATIONS" ("ID") 
+  ;
+--------------------------------------------------------
+--  DDL for Index ROLES_PK
+--------------------------------------------------------
+
+  CREATE UNIQUE INDEX "OA"."ROLES_PK" ON "OA"."ROLES" ("ROLE_ID") 
+  ;
+--------------------------------------------------------
+--  DDL for Index EMPLOYEES_PK
+--------------------------------------------------------
+
+  CREATE UNIQUE INDEX "OA"."EMPLOYEES_PK" ON "OA"."EMPLOYEES" ("ID") 
+  ;
+--------------------------------------------------------
+--  DDL for Index MENUS_PK
+--------------------------------------------------------
+
+  CREATE UNIQUE INDEX "OA"."MENUS_PK" ON "OA"."MENUS" ("MENU_ID") 
+  ;
+--------------------------------------------------------
+--  Constraints for Table DEPARTMENTS
+--------------------------------------------------------
+
+  ALTER TABLE "OA"."DEPARTMENTS" ADD CONSTRAINT "DEPARTMENT_PK" PRIMARY KEY ("DEPT_ID") ENABLE;
+  ALTER TABLE "OA"."DEPARTMENTS" MODIFY ("DEPT_ID" NOT NULL ENABLE);
+--------------------------------------------------------
+--  Constraints for Table ANNOUNCEMENTS
+--------------------------------------------------------
+
+  ALTER TABLE "OA"."ANNOUNCEMENTS" ADD CONSTRAINT "ANNOUNCEMENTS_PK" PRIMARY KEY ("ID") ENABLE;
+  ALTER TABLE "OA"."ANNOUNCEMENTS" MODIFY ("ID" NOT NULL ENABLE);
+--------------------------------------------------------
+--  Constraints for Table NOTIFICATIONS
+--------------------------------------------------------
+
+  ALTER TABLE "OA"."NOTIFICATIONS" ADD CONSTRAINT "NOTIFICATIONS_PK" PRIMARY KEY ("ID") ENABLE;
+  ALTER TABLE "OA"."NOTIFICATIONS" MODIFY ("ID" NOT NULL ENABLE);
+--------------------------------------------------------
+--  Constraints for Table ROLES
+--------------------------------------------------------
+
+  ALTER TABLE "OA"."ROLES" ADD CONSTRAINT "ROLES_PK" PRIMARY KEY ("ROLE_ID") ENABLE;
+  ALTER TABLE "OA"."ROLES" MODIFY ("ROLE_ID" NOT NULL ENABLE);
+--------------------------------------------------------
+--  Constraints for Table LOVS
+--------------------------------------------------------
+
+  ALTER TABLE "OA"."LOVS" ADD CONSTRAINT "LOVS_PK" PRIMARY KEY ("ID") ENABLE;
+  ALTER TABLE "OA"."LOVS" MODIFY ("ID" NOT NULL ENABLE);
+--------------------------------------------------------
+--  Constraints for Table EMPLOYEES
+--------------------------------------------------------
+
+  ALTER TABLE "OA"."EMPLOYEES" ADD CONSTRAINT "EMPLOYEES_PK" PRIMARY KEY ("ID") ENABLE;
+  ALTER TABLE "OA"."EMPLOYEES" MODIFY ("ID" NOT NULL ENABLE);
+  ALTER TABLE "OA"."EMPLOYEES" MODIFY ("PASSWORD" NOT NULL ENABLE);
+  ALTER TABLE "OA"."EMPLOYEES" MODIFY ("USER_NAME" NOT NULL ENABLE);
+--------------------------------------------------------
+--  Constraints for Table MENUS
+--------------------------------------------------------
+
+  ALTER TABLE "OA"."MENUS" ADD CONSTRAINT "MENUS_PK" PRIMARY KEY ("MENU_ID") ENABLE;
+  ALTER TABLE "OA"."MENUS" MODIFY ("MENU_ID" NOT NULL ENABLE);
+--------------------------------------------------------
+--  Constraints for Table ROLE_USERS
+--------------------------------------------------------
+
+  ALTER TABLE "OA"."ROLE_USERS" ADD CONSTRAINT "ROLE_USERS_PK" PRIMARY KEY ("ROLE_ID", "USER_ID") ENABLE;
+  ALTER TABLE "OA"."ROLE_USERS" MODIFY ("USER_ID" NOT NULL ENABLE);
+  ALTER TABLE "OA"."ROLE_USERS" MODIFY ("ROLE_ID" NOT NULL ENABLE);
+--------------------------------------------------------
+--  Constraints for Table LOCATIONS
+--------------------------------------------------------
+
+  ALTER TABLE "OA"."LOCATIONS" ADD CONSTRAINT "LOCATIONS_PK" PRIMARY KEY ("LOCATION_ID") ENABLE;
+  ALTER TABLE "OA"."LOCATIONS" MODIFY ("LOCATION_ID" NOT NULL ENABLE);
+--------------------------------------------------------
+--  Constraints for Table ROLE_MENUS
+--------------------------------------------------------
+
+  ALTER TABLE "OA"."ROLE_MENUS" ADD CONSTRAINT "ROLE_MENUS_PK" PRIMARY KEY ("ROLE_ID", "MENU_ID") ENABLE;
+  ALTER TABLE "OA"."ROLE_MENUS" MODIFY ("MENU_ID" NOT NULL ENABLE);
+  ALTER TABLE "OA"."ROLE_MENUS" MODIFY ("ROLE_ID" NOT NULL ENABLE);
+--------------------------------------------------------
+--  DDL for Trigger ANNOUNCEMENTS_TRG
+--------------------------------------------------------
+
+  CREATE OR REPLACE TRIGGER "OA"."ANNOUNCEMENTS_TRG" BEFORE INSERT ON ANNOUNCEMENTS 
+FOR EACH ROW 
+BEGIN
+  <<COLUMN_SEQUENCES>>
+  BEGIN
+    IF :NEW.ID IS NULL THEN
+      SELECT ANNOUNCEMENTS_SEQ.NEXTVAL INTO :NEW.ID FROM DUAL;
+    END IF;
+  END COLUMN_SEQUENCES;
+END;
+/
+ALTER TRIGGER "OA"."ANNOUNCEMENTS_TRG" ENABLE;
+--------------------------------------------------------
+--  DDL for Trigger DEPARTMENT_TRG
+--------------------------------------------------------
+
+  CREATE OR REPLACE TRIGGER "OA"."DEPARTMENT_TRG" BEFORE INSERT ON DEPARTMENTS 
+FOR EACH ROW 
+BEGIN
+  <<COLUMN_SEQUENCES>>
+  BEGIN
+    IF :NEW.DEPT_ID IS NULL THEN
+      SELECT DEPARTMENT_SEQ.NEXTVAL INTO :NEW.DEPT_ID FROM DUAL;
+    END IF;
+  END COLUMN_SEQUENCES;
+END;
+/
+ALTER TRIGGER "OA"."DEPARTMENT_TRG" ENABLE;
+--------------------------------------------------------
+--  DDL for Trigger EMPLOYEES_TRG
+--------------------------------------------------------
+
+  CREATE OR REPLACE TRIGGER "OA"."EMPLOYEES_TRG" BEFORE INSERT ON EMPLOYEES 
+FOR EACH ROW 
+BEGIN
+  <<COLUMN_SEQUENCES>>
+  BEGIN
+    IF :NEW.ID IS NULL THEN
+      SELECT EMPLOYEES_SEQ.NEXTVAL INTO :NEW.ID FROM DUAL;
+    END IF;
+  END COLUMN_SEQUENCES;
+END;
+/
+ALTER TRIGGER "OA"."EMPLOYEES_TRG" ENABLE;
+--------------------------------------------------------
+--  DDL for Trigger LOCATIONS_TRG
+--------------------------------------------------------
+
+  CREATE OR REPLACE TRIGGER "OA"."LOCATIONS_TRG" BEFORE INSERT ON LOCATIONS 
+FOR EACH ROW 
+BEGIN
+  <<COLUMN_SEQUENCES>>
+  BEGIN
+    IF :NEW.LOCATION_ID IS NULL THEN
+      SELECT LOCATIONS_SEQ.NEXTVAL INTO :NEW.LOCATION_ID FROM DUAL;
+    END IF;
+  END COLUMN_SEQUENCES;
+END;
+/
+ALTER TRIGGER "OA"."LOCATIONS_TRG" ENABLE;
+--------------------------------------------------------
+--  DDL for Trigger LOVS_TRG
+--------------------------------------------------------
+
+  CREATE OR REPLACE TRIGGER "OA"."LOVS_TRG" BEFORE INSERT ON LOVS 
+FOR EACH ROW 
+BEGIN
+  <<COLUMN_SEQUENCES>>
+  BEGIN
+    IF :NEW.ID IS NULL THEN
+      SELECT LOVS_SEQ.NEXTVAL INTO :NEW.ID FROM DUAL;
+    END IF;
+  END COLUMN_SEQUENCES;
+END;
+/
+ALTER TRIGGER "OA"."LOVS_TRG" ENABLE;
+--------------------------------------------------------
+--  DDL for Trigger NOTIFICATIONS_TRG
+--------------------------------------------------------
+
+  CREATE OR REPLACE TRIGGER "OA"."NOTIFICATIONS_TRG" BEFORE INSERT ON NOTIFICATIONS 
+FOR EACH ROW 
+BEGIN
+  <<COLUMN_SEQUENCES>>
+  BEGIN
+    IF :NEW.ID IS NULL THEN
+      SELECT NOTIFICATIONS_SEQ.NEXTVAL INTO :NEW.ID FROM DUAL;
+    END IF;
+  END COLUMN_SEQUENCES;
+END;
+/
+ALTER TRIGGER "OA"."NOTIFICATIONS_TRG" ENABLE;
+--------------------------------------------------------
+--  DDL for Trigger ROLES_TRG
+--------------------------------------------------------
+
+  CREATE OR REPLACE TRIGGER "OA"."ROLES_TRG" BEFORE INSERT ON ROLES 
+FOR EACH ROW 
+BEGIN
+  <<COLUMN_SEQUENCES>>
+  BEGIN
+    IF :NEW.ROLE_ID IS NULL THEN
+      SELECT ROLES_SEQ.NEXTVAL INTO :NEW.ROLE_ID FROM DUAL;
+    END IF;
+  END COLUMN_SEQUENCES;
+END;
+/
+ALTER TRIGGER "OA"."ROLES_TRG" ENABLE;
+
+
+CREATE OR REPLACE TYPE  "CHARTABLETYPE"  as table of varchar2(4000);
