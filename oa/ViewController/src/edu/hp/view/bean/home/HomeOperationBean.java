8eb689633d30e434dd7e9f30ff10ba9b1e1b1c06
Jin <xxx>
2013-02-07 12:55:42 +0800

    1

---
+++ oa/ViewController/src/edu/hp/view/bean/home/HomeOperationBean.java
@@ -0,0 +1,11 @@
+package edu.hp.view.bean.home;
+
+public class HomeOperationBean {
+    public HomeOperationBean() {
+    }
+
+    public String goToTaskPage() {
+        // Add event code here...
+        return null;
+    }
+}
