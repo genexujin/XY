ab72ce13fee9d28a0a9e043575975228f4002401
Jin <xxx>
2013-04-20 10:24:26 +0800

    commit

--- oa/Model/src/edu/hp/model/sms/SingletonSMSClient.java
+++ oa/Model/src/edu/hp/model/sms/SingletonSMSClient.java
@@ -8,10 +8,13 @@ import cn.emay.sdk.client.api.Client;
 import java.util.Date;
 
 public class SingletonSMSClient {
-    
+
     private static Client client = null;
+    //开发环境账号
     public static String password="220235";
     
+    //生成环境账号
+//    public static String password = "704168";
 
     private SingletonSMSClient() {
     }
@@ -39,11 +42,16 @@ public class SingletonSMSClient {
         return client;
     }
 
-//执行一次，激活账号
+    //执行一次，激活账号
+
     public static void main(String str[]) {
-        //Client theclient = SingletonSMSClient.getClient("0SDK-EBB-0130-NEVNN","222769");   
-        //theclient.sendSMS(new String[]{"18621910893","13817245519","13524173173"},"测试短信,发送时间： "+new Date()+",请查看是否收到及时 【祥韵公司】",1);
-        //System.err.println("ended!");
-        //theclient.registEx(password);
+//        Client theclient = SingletonSMSClient.getClient("3SDK-EMY-0130-OJXQS", "068075");
+//        int i = theclient.registEx(password);
+//        System.out.println("注册结果:" + i);
+//        //
+//        //        theclient.sendSMS(new String[] { "18621910893", "13817245519", "13524173173" },
+//        //                          "测试短信,发送时间： " + new Date() + ",请查看是否收到及时 【祥韵公司】", 1);
+//        System.err.println("ended!");
+//        theclient.registEx(password);
     }
 }
