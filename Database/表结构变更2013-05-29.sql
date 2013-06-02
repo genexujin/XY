97c2937bfc683c19d536b1c7d93235af99e60c56
Jin <xxx>
2013-06-02 14:38:30 +0800

    批量预订会议室表结构变化

--- Database/表结构变更2013-05-29.sql
+++ Database/表结构变更2013-05-29.sql
@@ -8,3 +8,12 @@
 Insert into OA.MENUS (MENU_ID,MENU_NAME,MENU_TASKFLOW_URL,MENU_DESC,MENU_CATEGORY,PARENT_MENU_ID,MENU_MASTER_CATEGORY,MENU_ICON_URL,EXPIRED,SEQ) values ('SYS_CAR_DRIVER','我的派车单','/WEB-INF/flows/vehicle/vehicle-app-driver-btf.xml#vehicle-app-driver-btf','司机派车单','CAR',null,'SYS','/images/icons/tasks.png','true','4');
 
 ---添加新的Role:司机
+
+-----VEHICLE_CALENDAR表添加一个新的字段
+   ALTER TABLE "OA"."PURCHASE_ORDERS" ADD USAGE_INFO varchar2(500);
+   ALTER TABLE "OA"."PURCHASE_ORDERS" ADD CURRENT_BUYER_ID varchar2(20);
+   ALTER TABLE "OA"."PURCHASE_ORDERS" ADD CURRENT_BUYER_NAME varchar2(40);
+
+   
+-----CONF_ROOM_CALENDAR 表添加一个新的字段
+ALTER TABLE "OA"."CONF_ROOM_CALENDAR" ADD CONF_LEVEL varchar2(20);
